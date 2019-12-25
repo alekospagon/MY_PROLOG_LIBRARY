fabce420144ce6f7ee705a8891dec4f3c310c545
alekospagon <56389540+alekospagon@users.noreply.github.com>
2019-12-25 05:57:41 -0500

    Update tree with fast index.pl

--- tree with fast index.pl
+++ tree with fast index.pl
@@ -2,13 +2,14 @@
  *
  * Each node contains slots forN elements and K subtrees.
  *
-*/
+ */
 
 %Tree parameters
-%choose power of 2 to avoid complex mod/div calculations
-n(10000).             %node elements
-k(10).                %subtrees hanging from node
-max_index(1000009).   %max index for my array.
+%choose power of 2 to avoid inefficient mod/div calculations
+n(1024).		%node elements  -> 2^13
+k(32).			%subtrees hanging from node -> 2^4
+log_k(5).
+max_index(1000009).	%max index for my array.
 
 %my handle of accessing
 access_N_plus_K(Index, Tree, Value) :-
@@ -19,44 +20,47 @@ access_N_plus_K(Index, Tree, Value) :-
 %accessing
 access_N_K(Index, Tree, Value) :-
 	%fetch N and K
-	n(N), k(K),
+	n(N), k(K), log_k(Log_K),
 	Sum is N+K,
 
 	%n_plus_k( ..... ) -> N+K arguments
 	functor(Tree, n_plus_k, Sum),
 
 	(
-	%Data if found here with Index <= N
+		%Data if found here with Index <= N
 		Index =< N ->
 		arg(Index, Tree, Value)
 
-	%I must go deeper in N_plus_K tree
-	;	
-    %with subtree? 
-    SonIndex is (Index - N - 1) mod K + N + 1,
-    %index on subtree
-		NewIndex is (Index - N - 1) //  K + 1,
-		arg(SonIndex, Tree, Son),
-    %go deeper
-		access_N_K(NewIndex, Son, Value)
-	).
+		%I must go deeper in N_plus_K tree
+		;	
+			%with subtree?
+			Sht_Idx is (Index - (N+1)),		%skip first N + 1 elems
+			%a mod b when b = 2^k is { a bitwise_and (k - 1) }
+			Mod_Idx is Sht_Idx /\ (K - 1),
+			SonIndex is Mod_Idx + N + 1,	%add back first N + 1 elems
+			%index on subtree
+			NewIndex is (Index - N - 1) >>  Log_K + 1,
+			arg(SonIndex, Tree, Son),
+			%go deeper
+			access_N_K(NewIndex, Son, Value)
+		).
 
 
 
-make_arr(Index, Tree) :-
-	%when max index-> Im done
-	max_index(Index) 
+	make_arr(Index, Tree) :-
+		%when max index-> Im done
+		max_index(Index) 
 	;
-	(
-	Index < 1000009,
-  %sample tree -> on third term store 
-  %your values. (I store the index)
-	access_N_plus_K(Index, Tree, Index),
-	Succ is Index + 1,
-	make_arr(Succ, Tree)
-	).
+		(
+			Index < 1000009,
+			%sample tree -> on third term store 
+			%your values. (I store the index)
+			access_N_plus_K(Index, Tree, Index),
+			Succ is Index + 1,
+			make_arr(Succ, Tree)
+		).
 
-%make a tree and fetch index
-indexing(Index, Result) :-
-	make_arr(1, Tree),
-	access_N_K(Index, Tree, Result).
+	%make a tree and fetch index
+	indexing(Index, Result) :-
+		make_arr(1, Tree),
+		access_N_plus_K(Index, Tree, Result).
